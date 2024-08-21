d9a87c3da6c3b080fe7948bbf7b8f4d16b544ab5
Emilio <115907759+Milo-IZ@users.noreply.github.com>
2024-08-21 16:41:03 -0600

    proyecto Django

---
+++ gestor_contenido/model/denuncia.py
@@ -0,0 +1,14 @@
+#-*- coding: utf-8 -*-
+
+from django.db import models
+
+class Denuncia(models.Model):
+    class Meta:
+        pass
+
+    contador = 0
+    
+
+    def registrar_denuncia(self, ):
+        pass
+
---
+++ gestor_contenido/model/materia.py
@@ -0,0 +1,16 @@
+#-*- coding: utf-8 -*-
+
+from django.db import models
+
+class Materia(models.Model):
+    class Meta:
+        pass
+
+    id = None
+    año = None
+    nombre = None
+
+
+    def agregar_contenido(self, contenido):
+        pass
+
