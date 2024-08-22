d607f564bf59ad558c0accab43cd97ac3482e910
Emilio <115907759+Milo-IZ@users.noreply.github.com>
2024-08-22 11:45:44 -0600

    Proyecto Actualizado

---
+++ investigaciones/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth.models import User
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html', {'form': AuthenticationForm()})
+    else:
+        name = request.POST["username"]
+        password = request.POST['password']
+        user = authenticate(username=name, password=password)
+        
+        if user is not None:
+            auth_login(request, user)  # Iniciar sesión
+            return redirect('foro/')  # Redirigir al foro
+        else:
+            return render(request, 'login.html', {'form': AuthenticationForm(), 'error': 'Usuario o contraseña incorrectos'})
+
+
+def register(request):
+    if request.method=='GET':
+        return render(request, 'register.html', {'form': UserCreationForm})
+    else:
+        
+        if request.POST["password1"]!=request.POST["password2"]:
+            return render(request, 'register.html', {'form': UserCreationForm, 'error': 'Las contraseñas no coinciden'})
+        else:
+            user = User.objects.create_user(request.POST["username"], password=request.POST["password1"])
+            user.save()
+            return render(request, 'register.html', {'form': UserCreationForm, 'valor': 'Usuario creado correctamente'})
+
+
+def foro(request):
+    
+    return render(request, "foro.html")
