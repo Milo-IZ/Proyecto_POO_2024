9cd9fcc9ac92717d976fe16dd97caf2a1430de88
Emilio <115907759+Milo-IZ@users.noreply.github.com>
2024-08-22 13:19:43 -0600

    sample de model

--- investigaciones/models.py
+++ investigaciones/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Contenido(models.Model):
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField(max_length=300)
+    contenido = models.TextField()
+    fecha = models.DateTimeField(auto_now_add=True)
+    autor= models.CharField(max_length=100)
+    imagen = models.URLField()
+    
+    
+
+    def __str__(self):
+        return self.titulo
