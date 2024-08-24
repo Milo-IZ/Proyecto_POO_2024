9b0b5af7eefb845ce1a296cfb86000ff819ad43c
Emilio <115907759+Milo-IZ@users.noreply.github.com>
2024-08-24 01:09:13 -0600

    proyecto 1.5

--- investigaciones/admin.py
+++ investigaciones/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Contenido
+from .models import Contenido, Comment, Denuncia
 
 admin.site.register(Contenido)
+admin.site.register(Comment)
+admin.site.register(Denuncia)
